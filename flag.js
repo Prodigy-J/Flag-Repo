eec897d4b3cf5b5915e82369632fcde11356472c
Prodigy-J <jknasumo@gmail.com>
2024-03-28 17:26:30 +0000

    first commit

---
+++ flag.js
@@ -0,0 +1,27 @@
+const country = document.querySelector("#country");
+const viewBtn = document.querySelector("button");
+const flagImage = document.querySelector("img");
+
+viewBtn.addEventListener("click", showFlag);
+flagImage.classList.add("hide");
+
+function showFlag() {
+  let countryName = country.value;
+  fetchCountryData(countryName);
+  setTimeout(() => {
+    flagImage.classList.remove("hide");
+  }, 1000);
+}
+
+async function fetchCountryData(country) {
+  const res = await fetch(
+    `https://countryinfoapi.com/api/countries/name/${country}`
+  );
+  const data = await res.json();
+  displayFlagFromData(data);
+}
+
+function displayFlagFromData(data) {
+  console.log(data);
+  flagImage.src = data.flag;
+}
